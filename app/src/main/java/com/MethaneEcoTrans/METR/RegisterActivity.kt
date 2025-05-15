60deed57f91c645668a8e23d73211a16890fccf5
degtiar gleb <glebsd22@gmail.com>
2025-05-15 16:35:43 +0300

    registration system done (it's finally wait until user click on verification link)

--- app/src/main/java/com/MethaneEcoTrans/METR/RegisterActivity.kt
+++ app/src/main/java/com/MethaneEcoTrans/METR/RegisterActivity.kt
@@ -5,40 +5,63 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.tasks.await
+
+suspend fun checkEmailVerification(
+    user: FirebaseUser,
+    onVerificationSuccess: (FirebaseUser) -> Unit,
+    onVerificationFailure: (String) -> Unit
+) {
+
+    // ждем подтверждения почты от пользователя в течение 1 минуты (60000 мс)
+    val startTime = System.currentTimeMillis()
+    val timeout = 60000L // 1 минута
+    val interval = 5000L // 5 секунд
+
+    while (System.currentTimeMillis() - startTime < timeout){
+        user.reload() // Обновляем информацию о пользователе
+        if (user.isEmailVerified) {
+            onVerificationSuccess(user) // Если подтверждено, вызываем успех
+            return
+        }
+        delay(interval)
+    }
+    // Если время вышло, удаляем пользователя
+    user.delete()
+    onVerificationFailure("Ошибка: пользователь не подтвердил свой email")
+}
 
 @Composable
 fun RegisterActivity(
     email: String,
     password: String,
-    onRegistrationSuccess: (FirebaseUser ) -> Unit,
+    onRegistrationSuccess: (FirebaseUser) -> Unit,
     onRegistrationFailure: (String) -> Unit
 ) {
     val auth = FirebaseAuth.getInstance()
 
     LaunchedEffect(Unit) {
         try {
-            // Регистрация пользователя
+            // Регистрируем пользователя в FireBas
             val result = auth.createUserWithEmailAndPassword(email, password).await()
+            // Создаем экземпляр user класса FirebaseUser дл использования его методов
             val user = result.user
 
             // Отправка письма с подтверждением
             user?.sendEmailVerification()?.await()
 
-            // Успешная регистрация
+            //ждем пока пользователь подтвердит почту
             if (user != null) {
                 UserData.uid = user.uid  // Получаем уникальный идентификатор пользователя
-                Log.d("RegisterActivity", "UID: ${UserData.uid}")
-
-
-                onRegistrationSuccess(user)
+                Log.d("RegisterActivity", "UID: ${UserData.uid}") // Логируем его
+                checkEmailVerification(user, onRegistrationSuccess, onRegistrationFailure)
             } else {
-                onRegistrationFailure("Ошибка: пользователь не создан.")
+                onRegistrationFailure("Ошибка: пользователь не создан")
             }
         } catch (e: Exception) {
             Log.e("RegisterActivity", "Ошибка регистрации: ${e.message}")
             onRegistrationFailure("Ошибка регистрации: ${e.message}")
         }
     }
-    userDataUpdate()
 }
